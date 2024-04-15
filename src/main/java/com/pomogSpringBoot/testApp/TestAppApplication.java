f6159cbc66564c9204469843a660c4cc492672db
Yurii Panasuik <thoryur@gmail.com>
2024-04-15 18:25:33 +0300

    mysql-connector-j

--- src/main/java/com/pomogSpringBoot/testApp/TestAppApplication.java
+++ src/main/java/com/pomogSpringBoot/testApp/TestAppApplication.java
@@ -1,7 +1,9 @@
 package com.pomogSpringBoot.testApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestAppApplication {
@@ -10,4 +12,11 @@ public class TestAppApplication {
 		SpringApplication.run(TestAppApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return  args -> {
+			System.out.println("CommandLineRunner");
+		};
+	
+	}
 }
