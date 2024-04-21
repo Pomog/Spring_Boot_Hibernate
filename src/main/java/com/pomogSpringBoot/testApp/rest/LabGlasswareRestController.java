8a2c9d2ce056bd5554e40b9de721abb3786954a9
Yurii Panasuik <thoryur@gmail.com>
2024-04-21 08:55:54 +0300

    PathVariable

--- src/main/java/com/pomogSpringBoot/testApp/rest/LabGlasswareRestController.java
+++ src/main/java/com/pomogSpringBoot/testApp/rest/LabGlasswareRestController.java
@@ -3,7 +3,9 @@ package com.pomogSpringBoot.testApp.rest;
 import com.pomogSpringBoot.testApp.entity.GlassJoint;
 import com.pomogSpringBoot.testApp.entity.JointType;
 import com.pomogSpringBoot.testApp.entity.LabGlassware;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class LabGlasswareRestController {
+    private List<LabGlassware> theLabGlassware;
     
-    @GetMapping("/labglassware")
-    public List<LabGlassware> getTestLabglassware (){
+    @PostConstruct
+    public void loadData(){
         var glassJoint1 = new GlassJoint(JointType.SPHERICAL_BALL, "S35");
         var glassJoint2 = new GlassJoint(JointType.SPHERICAL_CUP, "S41");
         var glassJoint3 = new GlassJoint(JointType.CONE_SOCKET, "29/32");
@@ -32,10 +35,22 @@ public class LabGlasswareRestController {
         labGlassware2.addGlassJoint(glassJoint3);
         
         List<LabGlassware> theLabGlassware = new ArrayList<>();
+        
         theLabGlassware.add(labGlassware1);
         theLabGlassware.add(labGlassware2);
         theLabGlassware.add(labGlassware3);
         
+        this.theLabGlassware = theLabGlassware;
+    }
+    
+    @GetMapping("/labglassware")
+    public List<LabGlassware> getTestLabglassware (){
         return theLabGlassware;
     }
+    
+    @GetMapping("/labglassware/{id}")
+    public LabGlassware getLabglasswareByID(@PathVariable int id){
+        return theLabGlassware.get(id);
+    }
+    
 }
