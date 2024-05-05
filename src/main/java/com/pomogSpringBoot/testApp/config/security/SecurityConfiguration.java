89b186f33710736d85a39abaa27c6729c50445fd
Yurii Panasuik <thoryur@gmail.com>
2024-05-05 16:39:10 +0300

    SecurityFilterChain

--- src/main/java/com/pomogSpringBoot/testApp/config/security/SecurityConfiguration.java
+++ src/main/java/com/pomogSpringBoot/testApp/config/security/SecurityConfiguration.java
@@ -3,9 +3,14 @@ package com.pomogSpringBoot.testApp.config.security;
 import com.pomogSpringBoot.testApp.config.UserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -23,6 +28,26 @@ public class SecurityConfiguration {
                 .roles(UserRole.MANAGER.name())
                 .build();
         
-        return new InMemoryUserDetailsManager(testUser, testManager);
+        UserDetails testAdmin = User.builder()
+                .username("Admin")
+                .password("{noop}12345")
+                .roles(UserRole.ADMIN.name())
+                .build();
+        
+        return new InMemoryUserDetailsManager(testUser, testManager, testAdmin);
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole(UserRole.USER.name(), UserRole.MANAGER.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.POST, "/api/**").hasAnyRole(UserRole.MANAGER.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.PUT, "/api/**").hasAnyRole(UserRole.MANAGER.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole(UserRole.ADMIN.name())
+        );
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
