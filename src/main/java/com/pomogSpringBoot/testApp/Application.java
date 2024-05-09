6a616a7cc01023c80bb19b07039e7eacfc87211f
Yurii Panasuik <thoryur@gmail.com>
2024-05-09 15:44:32 +0300

    Thymeleaf

--- src/main/java/com/pomogSpringBoot/testApp/Application.java
+++ src/main/java/com/pomogSpringBoot/testApp/Application.java
@@ -1,16 +1,10 @@
 package com.pomogSpringBoot.testApp;
 
 import com.pomogSpringBoot.testApp.dao.LabGlasswareDAO;
-import com.pomogSpringBoot.testApp.entity.GlassJoint;
-import com.pomogSpringBoot.testApp.entity.JointType;
-import com.pomogSpringBoot.testApp.entity.LabGlassware;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.sql.Date;
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
