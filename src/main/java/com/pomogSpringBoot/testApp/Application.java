0ddc509fd3f87e83febcc10e6b45e3d108a70264
Yurii Panasuik <thoryur@gmail.com>
2024-07-22 23:02:43 +0300

    My first Before advice ever

--- src/main/java/com/pomogSpringBoot/testApp/Application.java
+++ src/main/java/com/pomogSpringBoot/testApp/Application.java
@@ -1,6 +1,7 @@
 package com.pomogSpringBoot.testApp;
 
 import com.pomogSpringBoot.testApp.dao.LabGlasswareDAO;
+import com.pomogSpringBoot.testApp.entity.glassware.LabGlassware;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +17,12 @@ public class Application {
 	@Bean
 	public CommandLineRunner commandLineRunner(LabGlasswareDAO labGlasswareDAO){
 		return  args -> {
-
+			demoTheBeforeAdvice(labGlasswareDAO);
 		};
+	}
+	
+	private void demoTheBeforeAdvice(LabGlasswareDAO labGlasswareDAO) {
+		LabGlassware test = new LabGlassware("test", false, false);
+		labGlasswareDAO.save(test);
 	}
 }
---
+++ src/main/java/com/pomogSpringBoot/testApp/aspects/LoggingAspect.java
@@ -0,0 +1,14 @@
+package com.pomogSpringBoot.testApp.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    @Before("execution(public com.pomogSpringBoot.testApp.entity.glassware.LabGlassware save(com.pomogSpringBoot.testApp.entity.glassware.LabGlassware))")
+    public void beforeSaveAdvice(){
+        System.out.println("\n =============> execution @Before advice");
+    }
+}
